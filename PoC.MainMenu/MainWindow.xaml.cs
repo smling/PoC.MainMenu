3f694531ad66419b0d4ba963493bc62cc4945a96
凌 大少 <smling9_9@hotmail.com>
2017-04-09 07:27:31 +0800

    Add project files.

---
+++ PoC.MainMenu/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PoC.MainMenu
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void menuItemAddNewTab_Click(object sender, RoutedEventArgs e)
+        {
+            this.tabWorkingBench.Items.Add(new TabItem() { Header = "New Tab Item "+(tabWorkingBench.Items.Count-1).ToString() });
+        }
+    }
+}
---
+++ PoC.MainMenu/SideMenuWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PoC.MainMenu
+{
+    /// <summary>
+    /// Interaction logic for SideMenuWindow.xaml
+    /// </summary>
+    public partial class SideMenuWindow : Window
+    {
+        public SideMenuWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void menuItemAddNewTab_Click(object sender, RoutedEventArgs e)
+        {
+            this.tabWorkingBench.Items.Add(new TabItem() { Header = "New Tab Item " + (tabWorkingBench.Items.Count - 1).ToString() });
+        }
+    }
+}
